7a0dd517cb0f0ca1da662eb76ac60fb4e220b40b
SmirnovaAlina <alinas008@gmail.com>
2019-06-05 12:12:38 -0700

    complete optional part

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -18,7 +18,6 @@ function buildMetadata(sample) {
 
    });
 
-
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 };
@@ -67,6 +66,73 @@ function buildCharts(sample) {
   
 };
 
+function buildGauge(sample){
+   var url = "/wfreq/" + sample;
+  console.log(url);
+  // @TODO: Complete the following function that builds the metadata panel
+   d3.json(url).then((wfreq) => {
+    var level = wfreq[0]*19;
+    // Trig to calc meter point
+    var degrees = 180 - level,
+        radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.05 L .0 0.05 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    var data = [{ type: 'scatter',
+    x: [0], y:[0],
+        marker: {size: 12, color:'850000'},
+        showlegend: false,
+        name: 'Freq',
+        text: level,
+        hoverinfo: 'text+name'},
+    { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {
+        colors:[
+            'rgba(0, 105, 11, .5)', 'rgba(10, 120, 22, .5)',
+            'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+            'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+            'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+            'rgba(240, 230, 215, .5)', 'rgba(255, 255, 255, 0)']},
+    labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    hoverinfo: 'label',
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+    }];
+    var layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+            color: '850000'
+        }
+        }],
+    title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+    height: 500,
+    width: 500,
+    xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
+    // var GAUGE = document.getElementById('gauge');
+    Plotly.newPlot('gauge', data, layout);
+
+    });
+   };
+
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
@@ -85,6 +151,7 @@ function init() {
     console.log(firstSample)
     buildCharts(firstSample);
     buildMetadata(firstSample);
+    buildGauge(firstSample);
   });
 }
 
@@ -92,6 +159,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
+  buildGauge(newSample);
 }
 
 // Initialize the dashboard
